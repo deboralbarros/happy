552349963979a7e2cc5491183eb93992e42232cf
Débora Barros <debora_lbarros@outlook.com>
2020-10-12 16:21:10 -0400

    Iniciando o front-end

---
+++ web/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./styles/global.css";
+import "./styles/pages/landing.css";
+
+import Routes from "./routes";
+
+function App() {
+  return (
+    <Routes />
+  );
+}
+
+export default App;
